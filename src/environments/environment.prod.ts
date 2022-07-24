185564acc942bb58f1a620b6b41a3187882a13bf
istrejo <istrejo2106@gmail.com>
2022-07-24 10:59:01 -0400

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCObMwb3QC3RUQfjV9lrxqCu0S6q3xS9C8',
+    authDomain: 'portfolio-90497.firebaseapp.com',
+    projectId: 'portfolio-90497',
+    storageBucket: 'portfolio-90497.appspot.com',
+    messagingSenderId: '169767724283',
+    appId: '1:169767724283:web:ae727fb85c66630e3a3c5e',
+    measurementId: 'G-52WSVCG3QG',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCObMwb3QC3RUQfjV9lrxqCu0S6q3xS9C8',
+    authDomain: 'portfolio-90497.firebaseapp.com',
+    projectId: 'portfolio-90497',
+    storageBucket: 'portfolio-90497.appspot.com',
+    messagingSenderId: '169767724283',
+    appId: '1:169767724283:web:ae727fb85c66630e3a3c5e',
+    measurementId: 'G-52WSVCG3QG',
+  },
 };
 
 /*
